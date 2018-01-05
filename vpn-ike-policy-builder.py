4a5fdd95327c3d1d56182157e0fb64bb781416fb
Maliek Meersschaert <maliek.meersschaert@hotmail.com>
2018-01-05 11:20:53 +0100

    splitting files and proceed

--- vpn-ike-policy-builder.py
+++ vpn-ike-policy-builder.py
@@ -56,6 +56,7 @@ while True:
 		print entry, menu_ike_policy_cert_or_psc[entry]
 
 	selection_ike_policy_cert_or_psc = raw_input("Please Select:")
+
 	if selection_ike_policy_cert_or_psc == '1':
 		menu_ike_policy_cert = {}
 		menu_ike_policy_cert['1'] = "local-certificate"
@@ -97,14 +98,61 @@ while True:
 						continue
 			elif selection_ike_policy_cert == '3':
 				ike_policy_cert_custom = raw_input("set security ike policy " + ike_policy_name + " " + menu_ike_policy_cert_or_psc['1'] + " ")
-				print ike_policy_cert_custom
 				ike_proposal_policy_cert_command = "set security ike policy " + ike_policy_name + " " + menu_ike_policy_cert_or_psc['1'] + " " + ike_policy_cert_custom
 				set_commands['1'] = ike_proposal_policy_cert_command
 			else:
 				print "Unknown option selected"
 				continue
 			break
+
+
+	if selection_ike_policy_cert_or_psc == '2':
+		menu_ike_policy_psc = {}
+		menu_ike_policy_psc['1'] = "ascii-text"
+		menu_ike_policy_psc['2'] = "hexadecimal"
+
+		while True:
+			options_ike_policy_psc = menu_ike_policy_psc.keys()
+			options_ike_policy_psc.sort()
+			for entry in options_ike_policy_psc:
+				print entry, menu_ike_policy_psc[entry]
+
+			selection_ike_policy_psc = raw_input("Please Select:")
+			if selection_ike_policy_psc == '1':	
+				ike_policy_psc_ascii = raw_input("set security ike policy " + ike_policy_name + " " + menu_ike_policy_cert_or_psc['2'] + " " + menu_ike_policy_psc['1'] + " ")
+				ike_proposal_policy_psc_ascii_command = "set security ike policy " + ike_policy_name + " " + menu_ike_policy_cert_or_psc['2'] + " " + menu_ike_policy_psc['1'] + " " + ike_policy_psc_ascii
+				set_commands['1'] = ike_proposal_policy_psc_ascii_command
+				break
+			elif selection_ike_policy_psc == '2':
+				ike_policy_psc_hex = raw_input("set security ike policy " + ike_policy_name + " " + menu_ike_policy_cert_or_psc['2'] + " " + menu_ike_policy_psc['2'] + " ")
+				ike_proposal_policy_psc_hex_command = "set security ike policy " + ike_policy_name + " " + menu_ike_policy_cert_or_psc['2'] + " " + menu_ike_policy_psc['2'] + " " + ike_policy_psc_hex
+				set_commands['1'] = ike_proposal_policy_psc_hex_command
+				break
+	break
+
+menu_ike_policy_prop = {}
+menu_ike_policy_prop['1'] = "proposal-set"
+menu_ike_policy_prop['2'] = "proposals"
+
+
+while True:
+	options_ike_policy_prop = menu_ike_policy_prop.keys()
+	options_ike_policy_prop.sort()
+	for entry in options_ike_policy_prop:
+		print entry, menu_ike_policy_prop[entry]
+
+	selection_ike_policy_prop = raw_input("Please Select:")
+	if selection_ike_policy_prop == '1':
+		ike_policy_mode = "set security ike policy " + ike_policy_name + " mode " + menu_ike_policy_prop['1']
+		set_commands['0'] = ike_policy_mode
 		break
+	elif selection_ike_policy_prop == '2':
+		ike_policy_mode = "set security ike policy " + ike_policy_name + " mode " + menu_ike_policy_prop['2']
+		set_commands['0'] = ike_policy_mode
+		break
+	else:
+		print "Unknown option selected"
+		continue
 
 
 for entry in set_commands:
