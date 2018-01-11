9f6682b530d95d30a64d0483d334e011e23b4d71
Maliek Meersschaert <maliek.meersschaert@hotmail.com>
2018-01-11 09:56:38 +0100

    php skills trainin

---
+++ PHP/VPN/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/ike', function () {
+    return view('ike');
+});
+
+Route::get('/ipsec', function () {
+    return view('ipsec');
+});
